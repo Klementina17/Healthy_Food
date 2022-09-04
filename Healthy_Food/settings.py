d9417b07dbec000888480064a64cb768766a8a28
Klementina Dimitrova <klementina_dimitrova@live.com>
2022-09-04 22:55:34 +0200

    "Project"

--- Healthy_Food/settings.py
+++ Healthy_Food/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-v%xwzx1&rd%9-84!aipm)6k&#*t3k2kbg_836=df7qyq!(y$kg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','healthyfoodsapp.herokuapp.com/']
+ALLOWED_HOSTS = []
 
 
 # Application definition
